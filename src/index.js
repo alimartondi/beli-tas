c0463b4c16c92817823a5f16d2642366650c9c11
As'ad Saleh Umar <azzzad.s@gmail.com>
2020-10-31 22:03:33 +0700

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import functionStore from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
+const x = functionStore();
+const store = x.store;
+const persistor = x.persistor;
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<div>Loading</div>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,25 @@
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import rootReducer from "./reducers";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const logger = createLogger({
   collapsed: true,
 });
 
-export default createStore(rootReducer, applyMiddleware(logger));
+// export default createStore(rootReducer, applyMiddleware(logger));
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(logger));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
